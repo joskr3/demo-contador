f19f7615e2fac97d5da0a0b1dc8005f78036e55a
Josue Patricio <josretamozovar@gmail.com>
2025-01-17 20:43:23 -0500

    feat: refactor counter component to support two independent values and update display logic

--- src/components/contador/index.tsx
+++ src/components/contador/index.tsx
@@ -2,25 +2,25 @@ import { useState } from "react";
 
 function Contador() {
 
-  const [valor, setValor] = useState(0);
-
+  const [valor1, setValor1] = useState(0);
+    const [valor2, setValor2] = useState(0);
   return (
     <div className="container">
-      <h2>Contador: {valor}</h2>
+      <h2>Contador: {valor1 + valor2}</h2>
       <section>
         <label htmlFor="valor">Nuevo valor</label>
         <input
           type="text"
           id="valor"
           name="valor"
-          value={valor}
-          onChange={(event) => setValor(+event.target.value)}
+          value={valor1}
+          onChange={(event) => setValor1(+event.target.value)}
           placeholder="modificar valor"
         />
       </section>
       <section>
-        <button onClick={() => setValor((prevValue) => prevValue + valor)}>+</button>
-        <button onClick={() => setValor(valor - valor)}>-</button>
+        <button onClick={() => setValor2(valor2)}>+</button>
+        <button onClick={() => setValor()}>-</button>
         <button onClick={() => setValor(0)}>Reset</button>
       </section>
     </div>
